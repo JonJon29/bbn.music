af8df7438d85257adfa99cf29c313d7b3198ec70
Jonas Möller <jonas.moeller23@icloud.com>
2025-07-08 16:17:37 +0200

    fix: check date on change

--- pages/music/newDrop.ts
+++ pages/music/newDrop.ts
@@ -76,29 +76,33 @@ const validator = (page: number) => async () => {
         creationState.validationState.setValue(`${error.issues[0].path[0]}: ${error.issues[0].message}`);
         return;
     }
-    if(page == 1){
-        const releaseDate = new Date(creationState.release.value);
-        const releaseDateLimit = new Date();
-        releaseDateLimit.setDate(releaseDateLimit.getDate() + 14);
-        if(releaseDate < releaseDateLimit) {
-            sheetStack.addSheet(
-                Box(
-                    Label("Warning: ").setTextSize("xl").setFontWeight("bold").addStyle(css`
-                        :host {
-                        color: red;
-                    }
-                    `),
-                Label("Your release is in under 14 days!").setTextSize("lg").setFontWeight("bold"),
-                PrimaryButton("OK").onClick(() => sheetStack.removeOne()))
-            );
-        }
-    }
 
     creationState.validationState.setValue(undefined);
     await API.patchIdByDropsByMusic({ path: { id: dropId }, body: Object.fromEntries(Object.entries(creationState).map(([key, state]) => [key, state.value])) });
     creationState.page.setValue(page + 1);
     creationState.validationState.setValue(undefined);
 };
+
+const checkDate = () => {
+    const releaseDate = new Date(creationState.release.value);
+    const releaseDateLimit = new Date();
+    releaseDateLimit.setDate(releaseDateLimit.getDate() + 14);
+    if (releaseDate < releaseDateLimit) {
+        sheetStack.addSheet(
+            Grid(
+                SheetHeader("Warning", sheetStack),
+                Grid(
+                    Label("Your release date is less than 14 days away. Are you sure you want to continue?").setTextSize("lg"),
+                    PrimaryButton("Ok").onClick(() => sheetStack.removeOne())
+                ).setGap(),
+            )
+        );
+    }
+}
+
+async function saveDrop() {
+    await API.patchIdByDropsByMusic({ path: { id: dropId }, body: Object.fromEntries(Object.entries(creationState).map(([key, state]) => [key, state.value])) });
+}
 
 const footer = (page: number) =>
     Grid(
@@ -115,6 +119,8 @@ creationState.primaryGenre.listen((_, old) => {
         creationState.secondaryGenre.setValue(undefined);
     }
 });
+
+creationState.release.listen((_, old) => {checkDate()})
 
 const wizard = creationState.page.map((page) => {
     if (page == 0) {
